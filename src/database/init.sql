78199a6e62b85a9f39b15e9f8c08b23fcd7c1c38
Dev Joseph <esseyjoseph9@gmail.com>
2025-06-16 08:55:59 +0000

    Add of init.sql

---
+++ src/database/init.sql
@@ -0,0 +1,10 @@
+-- database/init.sql
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  firstname VARCHAR(255) NOT NULL,
+  lastname VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  role VARCHAR(30),
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
