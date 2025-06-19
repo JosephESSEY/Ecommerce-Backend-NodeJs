ea2136aad8290fc5109d8bebb0884fb8f54dc08c
Dev Joseph <esseyjoseph9@gmail.com>
2025-06-19 06:03:40 +0000

    users-feature

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const { createUser, findUserByEmail } = require("../models/user.model");
+const { createUser, findUserByEmail, getAllUsers, getUserProfile, updateUser, deleteUser, findUserById } = require("../models/user.model");
 
 const register = async (req, res) => {
   try {
@@ -30,7 +30,7 @@ const login = async (req, res) => {
     const valid = await bcrypt.compare(password, user.password);
     if (!valid) return res.status(400).json({ error: "Incorrect password" });
 
-    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+    const token = jwt.sign({ userId: user.id, userRole: user.role }, process.env.JWT_SECRET, { expiresIn: "1d" });
 
     res.status(200).json({ token });
   } catch (err) {
@@ -39,7 +39,60 @@ const login = async (req, res) => {
   }
 };
 
+
+const getAllUsersController = async (req, res) => {
+  try {
+    const users = await getAllUsers();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error : error});
+  }
+};
+
+
+const updateUserController = async (req, res) => {
+  try {
+    const existingUser = await findUserById(req.params.id);
+    if(!existingUser) return res.status(404).json({ error: "User not found" });
+
+    const { email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const updated = await updateUser(req.params.id, { email, password: hashedPassword });
+    res.status(201).json({
+      message: "User updated successfully",
+      result: updated
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Erreur serveur" });
+  }
+};
+
+const deleteUserController = async (req, res) => {
+  try {
+    const existingUser = await findUserById(req.params.id);
+    if(!existingUser) return res.status(404).json({ error: "User not found"})
+
+    await deleteUser(req.params.id);
+    res.json({ message: "User delected successfuly" });
+  } catch (err) {
+    res.status(500).json({ message: "Erreur serveur" });
+  }
+};
+
+const getProfileController = async (req, res) => {
+  try {
+    const user = await getUserProfile(req.user.userId);
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: "Erreur serveur" });
+  }
+};
+
 module.exports = {
   register,
   login,
+  getAllUsersController,
+  getProfileController,
+  updateUserController,
+  deleteUserController
 };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -20,7 +20,43 @@ const findUserByEmail = async (email) => {
   return rows[0];
 };
 
+const getAllUsers = async () => {
+  const result = await pool.query("SELECT id, firstname, lastname, email, role FROM users");
+  return result.rows;
+};
+
+
+const updateUser = async (id, { email, password }) => {
+  const result = await pool.query(
+    "UPDATE users SET email = $1, password = $2 WHERE id = $3 RETURNING id, email",
+    [email, password, id]
+  );
+  return result.rows[0];
+};
+
+const deleteUser = async (id) => {
+  await pool.query("DELETE FROM users WHERE id = $1", [id]);
+};
+
+const getUserProfile = async (id) => {
+  const result = await pool.query("SELECT id, firstname, lastname, email, role FROM users WHERE id = $1", [id]);
+  return result.rows[0];
+};
+
+const findUserById = async (id) => {
+  const query = `SELECT * FROM users WHERE id = $1`;
+  const value = [id];
+  const { rows } = await pool.query(query, value);
+  return rows[0];
+}
+
+
 module.exports = {
   createUser,
   findUserByEmail,
+  getAllUsers,
+  updateUser,
+  deleteUser,
+  getUserProfile,
+  findUserById
 };
