c1d37bf674ff9a1e5d7f33cc25296d5f8ca98ed1
Dev Joseph <esseyjoseph9@gmail.com>
2025-06-20 23:39:48 +0000

    Cart & CartLines Management

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -12,10 +12,80 @@ const getUserCart = async (user_id) => {
     const query = `SELECT * FROM cart WHERE user_id = $1`;
     const value = [user_id];
     const execute = await pool.query(query, value);
+    return execute.rows[0];
+}
+
+const addProductCart = async (cart_id, product_id, price) => {
+    const query = `INSERT INTO cart_lines(cart_id, product_id, quantity, price)
+    VALUES($1, $2, $3, $4) RETURNING *`;
+    const values = [cart_id, product_id, 1, price];
+    const execute = await pool.query(query, values);
+    return execute.rows[0];
+}
+
+const verifyProductCart = async (cart_id, product_id) => {
+    const query = `SELECT * FROM cart_lines WHERE cart_id = $1 AND product_id = $2`;
+    const values = [cart_id, product_id];
+    const execute = await pool.query(query, values);
+    return execute.rows[0];
+}
+
+const setQuantity = async (cart_id, product_id, quantity) => {
+    const query = `UPDATE cart_lines
+    SET quantity = $1
+    WHERE cart_id = $2 AND product_id = $3 RETURNING *`;
+    const values = [quantity, cart_id, product_id];
+    const execute = await pool.query(query, values);
+    return execute.rows[0];
+}
+
+const setPrice = async (cart_id, product_id, price) => {
+    const query = `UPDATE cart_lines
+    SET price = $1
+    WHERE cart_id = $2 AND product_id = $3 RETURNING *`;
+    const values = [price, cart_id, product_id];
+    const execute = await pool.query(query, values);
+    return execute.rows[0];
+}
+
+const getQuantity = async (cart_id, product_id) => {
+    const query = `SELECT quantity 
+    FROM cart_lines
+    WHERE cart_id = $1 AND product_id = $2`;
+    const values = [cart_id, product_id];
+    const execute = await pool.query(query, values);
+    return execute.rows[0];
+}
+
+const deleteProductInCart = async (cart_id, product_id) => {
+    const query = `DELETE FROM cart_lines
+    WHERE cart_id = $1 AND product_id = $2 RETURNING *`;
+    const values = [cart_id, product_id];
+    const execute = await pool.query(query, values);
+    return execute.rows[0];
+}
+
+const cartModel = async (user_id) => {
+    const query = `SELECT P.id as product_id, P.name as product_name, P.price as unique_price, L.price as prix_total, L.quantity, C.id as card_id
+    FROM cart_lines L
+    JOIN cart C ON C.id = L.cart_id
+    JOIN products P ON p.id = L.product_id
+    JOIN users U ON U.id = C.user_id
+    WHERE U.id = $1`;
+    const value = [user_id];
+    const execute = await pool.query(query, value);
     return execute.rows;
 }
+
 
 module.exports = {
     addCart,
     getUserCart,
+    addProductCart,
+    verifyProductCart,
+    setQuantity,
+    getQuantity,
+    deleteProductInCart,
+    cartModel,
+    setPrice
 }
